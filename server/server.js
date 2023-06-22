8e9a28951b92a28492a736e094ea02c079f5ce47
ganbari <steinway.nathan@gmail.com>
2023-06-22 10:59:42 -0500

    sets up project

---
+++ server/server.js
@@ -0,0 +1,28 @@
+const express = require('express')
+const cors = require('cors')
+const path = require('path')
+
+const app = express()
+
+app.use(express.json())
+app.use(cors())
+
+// allows you to serve static files
+app.use(express.static())
+
+// sendFile.join is combining the current directory name with the specified url
+// This is useful because we can serve files to our front end
+app.get('/', (req, res) => {
+    res.status(200).sendFile(path.join(__dirname, '../public/index.html'))
+})
+
+app.get('/castle', (req, res) => {
+    res.status(200).send(`<h1>Welcome to my castle!</h1>`)
+})
+
+app.get('/profile', (req, res) => {
+    res.status(200).sendFile(path.join(__dirname, '../public/profile.html'))
+})
+
+
+app.listen(4000, console.log('Wh- wha? What time is it?'))
